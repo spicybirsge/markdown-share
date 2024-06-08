80e92cc2230a994090bcda2875d57f0c0bbad332
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-06-08 20:41:28 +0530

    updates

--- frontend/src/providers/provider.jsx
+++ frontend/src/providers/provider.jsx
@@ -3,5 +3,5 @@ import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes'
 
 export function Provider({ children }) {
-    return <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">{children}</Theme>
+    return <Theme  radius="large" scaling="95%">{children}</Theme>
 }
