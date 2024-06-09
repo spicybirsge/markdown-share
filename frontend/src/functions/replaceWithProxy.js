df6311e8e460e3c1ff500fac5bb9a36ec96c430e
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-06-09 23:03:21 +0530

    fix editor bug

--- frontend/src/functions/replaceWithProxy.js
+++ frontend/src/functions/replaceWithProxy.js
@@ -5,13 +5,17 @@ export default function replaceWithProxy(markdownText, proxyUrl = variables.BACK
   const imagePattern = /!\[(.*?)\]\((.*?)\)/g;
   const imgTagPattern = /<img\s+[^>]*src="([^"]*)"[^>]*>/g;
 
- 
   let sanitizedText = markdownText.replace(imagePattern, (match, altText, imageUrl) => {
+    if (imageUrl.startsWith(proxyUrl)) {
+      return match;
+    }
     return `![${altText}](${proxyUrl}${encodeURIComponent(imageUrl)})`;
   });
 
-
   sanitizedText = sanitizedText.replace(imgTagPattern, (match, imageUrl) => {
+    if (imageUrl.startsWith(proxyUrl)) {
+      return match;
+    }
     const newTag = match.replace(imageUrl, `${proxyUrl}${encodeURIComponent(imageUrl)}`);
     return newTag;
   });
